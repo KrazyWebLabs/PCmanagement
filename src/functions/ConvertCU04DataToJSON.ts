bf4dbe8722f0c6962cce9b1210d2a3731c470380
Monarca117 <skyggemonarken@gmail.com>
2024-11-11 09:41:44 -0600

    Visualización de tablas

---
+++ src/functions/ConvertCU04DataToJSON.ts
@@ -0,0 +1,17 @@
+import type { ConvertedData } from "@customTypes/SouvenirsData";
+
+export function convertSouvenirsDataToJSON(data: DBData): ConvertedData[] {
+  return data.rows.map((row: (string | number | null)[]) => {
+    return {
+      souvenirID: Number(row[0]),
+      souvenirName: row[1] as string,
+      priceWithoutIVA: Number(row[2]),
+      IVA: Number(row[3]),
+      priceWithIVA: Number(row[4]),
+      description: row[5] as string,
+      warehouseID: String(row[6]).toLowerCase()  , 
+      statusID: String(row[7]).toLowerCase(),    
+    };
+  });
+};
+
