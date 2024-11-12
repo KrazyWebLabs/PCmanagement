8913b8230dbc8c512d63a1626b9637c22dcdb0a5
Monarca117 <skyggemonarken@gmail.com>
2024-11-12 10:30:34 -0600

    Helouda

--- src/functions/ConvertSuppliersDataToJSON.ts
+++ src/functions/ConvertSuppliersDataToJSON.ts
@@ -4,12 +4,13 @@ export function convertSouvenirsDataToJSON(data: DBData): ConvertedData[] {
   return data.rows.map((row: (string | number | null)[]) => {
     return {
         supplierID: Number(row[0]),
-        legalEntityID: Number(row[1]),
+        legalEntityID: Number(row[10]),
+        legalText: String(row[1]).toLowerCase(),
         addressID: Number(row[2]),
         address: row[2] as string,         // Dirección (nombre o detalles)
         phoneNumber: row[3] as string,
         email: row[4] as string,
-        statusID: String(row[5]).toLowerCase(),
+        statusID: String(row[7]).toLowerCase(),
         statusName: String(row[5]).toLowerCase(),     
     };
   });
--- src/types/suppliersData.ts
+++ src/types/suppliersData.ts
@@ -10,6 +10,7 @@ export type SuppliersData = {
 export interface ConvertedData {
     supplierID: number;
     legalEntityID: number;
+    legalText: string;
     addressID: number;
     address: string;        
     phoneNumber: string;
