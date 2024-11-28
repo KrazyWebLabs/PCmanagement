08c2b0c30034a6c4abe0bd8c446c4e05b76185ea
Monarca <ruin002016@gmail.com>
2024-11-28 10:42:54 -0600

    Falta addresses

--- src/types/CustomersData.ts
+++ src/types/CustomersData.ts
@@ -1,12 +1,20 @@
 export interface ConvertedCustomerData {
-    customerID: number;
-    CURP: string;
-    firstName: string;
-    paternalLastName : string;
-    maternalLastName: string;
-    phoneNumber: string;
-    Email: string;
-    RFC: string;
-    address: string;
-    status:  string;
-  }
+  customerID: number;
+  CURP: string;
+  firstName: string;
+  paternalLastName: string;
+  maternalLastName: string;
+  phoneNumber: string;
+  Email: string;
+  RFC: string;
+  country: string;
+  state: string;
+  neighborhood: string;
+  postalCode: string;
+  street: string;
+  outsideNumb: number;
+  city: string;
+  addressID: string;
+  statusID: string;
+  statusName: string;
+}
