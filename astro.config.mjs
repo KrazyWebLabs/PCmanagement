76068eb6ef7807244887c92e24cb407d96b4e6c5
CodingGaby <macma.cube1@gmail.com>
2024-12-10 14:31:32 -0600

    feat(app): db added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import tailwind from '@astrojs/tailwind';
 
 import vercel from '@astrojs/vercel/serverless';
 
+import db from '@astrojs/db';
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: false,
   output: "server",
-  integrations: [tailwind()],
+  integrations: [tailwind(), db()],
   adapter: vercel()
 });
