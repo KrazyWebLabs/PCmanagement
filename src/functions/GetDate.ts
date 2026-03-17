228940c4c163867dcdb08da2b45b6a3c2148b582
CodingGaby <macma.cube1@gmail.com>
2026-03-17 02:30:08 -0600

    feat(app): Reports Added ✅
    Reports and freeAll button added

--- src/functions/GetDate.ts
+++ src/functions/GetDate.ts
@@ -1,5 +1,16 @@
 export default function getDate() {
-  return new Date().toLocaleString("es-MX", {
-    timeZone: "America/Mexico_City",
-  });;
+  const now = new Date();
+    
+  const localISO = new Intl.DateTimeFormat('sv-SE', {
+    timeZone: 'America/Mexico_City',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false
+  }).format(now).replace(' ', ' ');
+ 
+  return localISO;
 }
